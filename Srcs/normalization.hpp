774870e06e93673ce580bc3347375804872d11cf
Omar Adel <OmarAdel.connect@gmail.com>
2026-05-06 05:51:16 +0300

    Fix Priority of Conditions and merge overlap of conditions

--- Srcs/normalization.hpp
+++ Srcs/normalization.hpp
@@ -1,22 +1,43 @@
 #ifndef NORMALIZATION_HPP
 #define NORMALIZATION_HPP
 #include <bits/stdc++.h>
-class Normalization {
+using namespace std;
+
+class Normalization
+{
 private:
     string initial;
-public:
-    Normalization(string initial) {
-        this->initial = initial;
-        for (int i=0; i<initial.length(); i++) {
-            if (initial[i]>='0' && initial[i]<='9') continue;
-            if (initial[i]=='.'||initial[i]=='-'||initial[i]=='_'||initial[i]=='/')initial[i]='.';
-            if (initial[i]=='!'||initial[i]=='@'||initial[i]=='#'||initial[i]=='$')initial[i]='@';
-            if (initial[i]=='%'||initial[i]=='^'||initial[i]=='&'||initial[i]=='*')initial='&';
-            if (initial[i]>='a'&&initial[i]<='z')continue;
-            if (initial[i]>='A'&&initial[i]<='Z')initial[i]=(initial[i]-'A'+'a');
 
+public:
+    Normalization() = default;
+    Normalization(string initial)
+    {
+        this->initial = initial;
+        for (int i = 0; i < initial.length(); i++)
+        {
+            if ((initial[i] >= '0' && initial[i] <= '9') || (initial[i] >= 'a' && initial[i] <= 'z'))
+                continue;
+
+            if (initial[i] >= 'A' && initial[i] <= 'Z')
+            {
+                initial[i] = (initial[i] - 'A' + 'a');
+            }
+            else if (initial[i] == '.' || initial[i] == '-' || initial[i] == '_' || initial[i] == '/')
+            {
+                initial[i] = '.';
+            }
+            else if (initial[i] == '!' || initial[i] == '@' || initial[i] == '#' || initial[i] == '$')
+            {
+                initial[i] = '@';
+            }
+            else if (initial[i] == '%' || initial[i] == '^' || initial[i] == '&' || initial[i] == '*')
+            {
+                initial[i] = '&';
+            }
         }
     };
-    Normalization();
+
+    string get() { return this->initial; }
 };
+
 #endif
