8cb5a168816e4fc55c12e617448522c40d0d5767
Omar Adel <OmarAdel.connect@gmail.com>
2026-05-06 05:58:00 +0300

    fix spaces and add more comments to make TRIENODE understandable

--- Srcs/node.h
+++ Srcs/node.h
@@ -1,20 +1,27 @@
 #ifndef SM_OSINT_NODE_H
 #define SM_OSINT_NODE_H
 #include <cstring>
-class TRIENODE {
+
+class TRIENODE
+{
 public:
-    TRIENODE* children[40];
+    TRIENODE *children[40];
     int pfx, end;
-    TRIENODE() {
+
+    TRIENODE()
+    {
         pfx = 0;
         end = 0;
         memset(children, 0, sizeof(children));
     }
-    ~TRIENODE() {
-        for (int i=0; i<40; i++) {
-            if (children[i]) delete children[i];
+
+    ~TRIENODE() // memory deallocation
+    {
+        for (int i = 0; i < 40; i++)
+        {
+            if (children[i])
+                delete children[i];
         }
     }
-
 };
 #endif
