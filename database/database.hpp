60ce7e4cc9794d7d514eef3e7c40f6d518944da9
Omar Adel <OmarAdel.connect@gmail.com>
2026-06-01 21:18:50 +0300

    rename file from .h to .hpp

---
+++ database/database.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <mysql_connection.h>
+#include <mysql_driver.h>
+
+// The main data of users
+struct user
+{
+    int id;
+    std::string username;
+    std::string normalized;
+};
+
+class Database
+{
+private:
+    // MySQL driver && connection
+    sql::Driver *driver;
+    std::unique_ptr<sql::Connection> con;
+
+public:
+    Database();
+
+    bool connect(const std::string &host, const std::string &user, const std::string &password, const std::string &database);
+
+    void disconnect();
+
+    void InsertUser(const std::string &usernmae, const std::string &normalized);
+
+    std::vector<user> GetUsers();
+};
