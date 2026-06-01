9fc88d1fad6ba41feb4015c0b0187c0a13199d4b
Omar Adel <OmarAdel.connect@gmail.com>
2026-06-01 21:45:54 +0300

    add main.cpp and init the connection's data

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "database.hpp"
+
+// database connection's data
+const std::string host = "tcp://127.0.0.1:3306";
+const std::string user = "osinit";
+const std::string password = "sm_osinit";
+const std::string database = "osinit";
+
+int main()
+{
+    Database db;
+
+    return 0;
+}
